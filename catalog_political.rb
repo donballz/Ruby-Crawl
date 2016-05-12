61b27b3227051a31ff31c98be36a28dc1c57b31c
Donald Armstead <donald.armstead@gmail.com>
2016-05-12 01:41:28 -0400

    get_thread_list complete

--- catalog_political.rb
+++ catalog_political.rb
@@ -1,6 +1,13 @@
 require_relative 'readThread.rb'
 require_relative 'StringFind.rb'
 
+STICKY = 280594
+
+def write(obj, fname)
+	path = '/Users/donald/Dropbox/AO Thread Crawl/Ruby Port/'
+	File.open(path + "#{fname}.yml", 'w') { |f| f.write obj.to_yaml }
+end
+
 def get_page(url)
 	return Net::HTTP.get(URI.parse(url))
 end
@@ -15,7 +22,23 @@ end
 
 def get_thread_list(fnum)
 	# function crawls given subforum and returns array of threads
-	
+	tlist = []
+	thread, pnum, found = STICKY, 1, 0
+	page = get_page(build_url(fnum, pnum))
+	until tlist.include?(thread)
+		# puts in list unless already on list. also ignores the sticky
+		tlist.push(thread) unless thread == STICKY
+		page = get_page(build_url(fnum, pnum)) if found == -1
+		found = page.find('<td class="alt1" id="td_threadtitle_', found)
+		end_tnum = page.index('"', found)
+		if found == -1
+			pnum += 1
+			thread = STICKY
+		else
+			thread = page[found...end_tnum].to_i
+		end
+	end
+	return tlist
 end
 	
-puts get_page(build_url(23, 2))
+write(get_thread_list(23), 'thread_list')

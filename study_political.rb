0e1c20fcceda1ee3237545c5d471384e9bfa47d8
Donald Armstead <donald.armstead@gmail.com>
2016-05-17 21:43:27 -0400

    begin per year studies

--- study_political.rb
+++ study_political.rb
@@ -5,15 +5,27 @@ require_relative 'common_funcs.rb'
 def posts_per_year(fnum)
 	# returns hash of posts by year for given subforum
 	tcat = read("thread_cat_#{fnum}")
+	ppy = Hash.new(0)
 	tcat.each do |k, v|
 		if v > 0
-			ppy = Hash.new(0)
 			mt = read("Threads/#{k}")
-			puts mt.tTitle
-			mt.each { |post| puts mt.tPosts.index(post) + 1 if post.pYear == 1779 }
-			break
+			mt.each { |post| ppy[post.pYear] += 1 }
 		end
 	end
+	return ppy
+end
+
+def threads_per_year(fnum)
+	# returns hash of threads per year for given subforum
+	tcat = read("thread_cat_#{fnum}")
+	tpy = Hash.new(0)
+	tcat.each do |k, v|
+		if v > 0
+			mt = read("Threads/#{k}")
+			tpy[mt.tYear] += 1
+		end
+	end
+	return tpy
 end
 
 def ttesting(thread)
@@ -22,5 +34,8 @@ def ttesting(thread)
 	mt.each { |post| puts post.pTime }
 end
 
-#posts_per_year(23)
+now = Time.now
+#puts posts_per_year(23)
+puts threads_per_year(23)
 #ttesting(308671)
+puts "Run time: #{Time.now - now}"

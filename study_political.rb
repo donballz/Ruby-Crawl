bc11fc47eb68d9072e49db2445bcc9987985cf60
Donald Armstead <donald.armstead@gmail.com>
2016-05-17 21:51:49 -0400

    combine to one scan for time purpose

--- study_political.rb
+++ study_political.rb
@@ -2,30 +2,30 @@ require_relative 'readThread.rb'
 require_relative 'StringFind.rb'
 require_relative 'common_funcs.rb'
 
-def posts_per_year(fnum)
+def annual_hash
+	# return hash of blank hashes by year
+	mh = {}
+	[2001...2016].each { |y| mh[y] = Hash.new(0) }
+	return mh
+end
+
+def per_year_stats(fnum)
 	# returns hash of posts by year for given subforum
 	tcat = read("thread_cat_#{fnum}")
 	ppy = Hash.new(0)
-	tcat.each do |k, v|
-		if v > 0
-			mt = read("Threads/#{k}")
-			mt.each { |post| ppy[post.pYear] += 1 }
-		end
-	end
-	return ppy
-end
-
-def threads_per_year(fnum)
-	# returns hash of threads per year for given subforum
-	tcat = read("thread_cat_#{fnum}")
 	tpy = Hash.new(0)
+	top_posters = annual_hash
 	tcat.each do |k, v|
 		if v > 0
 			mt = read("Threads/#{k}")
-			tpy[mt.tYear] += 1
+			tpy[mt.tPosts[0].tYear] += 1
+			mt.each do |post| 
+				ppy[post.pYear] += 1 
+				top_posters[post.pYear][post.pPoster] += 1
+			end
 		end
 	end
-	return tpy
+	return tpy, ppy
 end
 
 def ttesting(thread)
@@ -36,6 +36,9 @@ end
 
 now = Time.now
 #puts posts_per_year(23)
-puts threads_per_year(23)
+tpy, ppy, tp = threads_per_year(23)
+puts tpy
+puts ppy
+puts tp
 #ttesting(308671)
 puts "Run time: #{Time.now - now}"

6932dfebdda350a68e326317e6d189d258db33ff
Donald Armstead <donald.armstead@gmail.com>
2016-07-01 02:04:03 -0400

    feat: added parse history class

--- threadDict.rb
+++ threadDict.rb
@@ -53,3 +53,49 @@ class ThreadDict
 		end
 	end
 end
+
+class ParseHistory
+	# class to track parse history
+	ATTRS = [:updateTimes, :threadTimes, :threadPosts]
+	attr_reader(*ATTRS)
+	
+	def initialize(fnum, init)
+		@fNum = fnum
+		@updateTimes = []
+		@threadTimes = {}
+		@threadPosts = {}
+		build_history(fnum, init)
+	end
+	
+	def write
+		# write yaml file of this object
+		path = '/Users/donald/Dropbox/AO Thread Crawl/Ruby Port/'
+		File.open(path + "parse_history_#{@fNum}.yml", 'w') { |f| f.write self.to_yaml }
+	end
+	
+	def build_history(fnum, init)
+		# the real initialize method
+		tcat = read("thread_cat_#{fnum}")
+		tcat.each do |k, v|
+			if v > 0
+				# political initial time:
+				#init = Time.new(2016,06,24,21,14,0)
+				@updateTimes.push(init)
+				@threadTimes[k] = [init]
+				@threadPosts[k] = [v]
+			end
+		end
+	end
+	
+	def update(thread, posts, time)
+		# func to add/update 
+		@updateTimes.push(time) unless @updateTimes.include?(time)
+		if @threadPosts.include?(thread)
+			@threadTimes[thread].push(time)
+			@threadPosts[thread].push(posts)
+		else
+			@threadTimes[thread] = [time]
+			@threadPosts[thread] = [posts]
+		end
+	end
+end

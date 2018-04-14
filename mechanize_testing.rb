dd4d71d460a9d69b7deb78063c55ddde56a074ee
Donald Armstead <donald.armstead@gmail.com>
2018-04-14 02:44:12 -0400

    feat: working AO login with Mechanize ACTUAL

---
+++ mechanize_testing.rb
@@ -0,0 +1,23 @@
+require 'rubygems'
+require 'mechanize'
+
+agent = Mechanize.new
+page = agent.get('http://www.actuarialoutpost.com/actuarial_discussion_forum/index.php')
+login = page.form_with(:action => "login.php?do=login")
+login.vb_login_username = 'ADoggieDetective'
+login.vb_login_password = 'H2A5cVQzT28wCLx#'
+agent.submit(login, login.buttons.first)
+
+pol = agent.get('http://www.actuarialoutpost.com/actuarial_discussion_forum/forumdisplay.php?f=23')
+pp pol
+
+#pp pol.links_with(:text => 'showthread.php?t=')
+#threads = []
+#pol.links.each do |l|
+	#l.uri.to_s
+	#if l.uri.to_s.find('showthread.php?t=') != -1
+	#	threads.push(l)
+	#end
+#end
+
+#threads.each { |t| pp t }

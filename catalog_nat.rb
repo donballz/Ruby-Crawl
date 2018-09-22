146d0e384f8b320774aeddb813fa31e52843765c
Donald Armstead <donald.armstead@gmail.com>
2018-09-21 23:13:35 -0400

    chore: after first NAT scrape

--- catalog_nat.rb
+++ catalog_nat.rb
@@ -7,7 +7,11 @@ fnum = 22
 time = Time.now
 #write(get_thread_list(fnum), "thread_list_#{fnum}")
 #write(update_tlist(fnum), "tllist_update_#{fnum}")
-tlist, replies = update_tlist(fnum, 10)
+#tlist, replies = update_tlist(fnum, 1000)
+#write(tlist, "thread_list_#{fnum}")
+#write(replies, "replies_#{fnum}")
+tlist = read("thread_list_#{fnum}")
+replies = read("replies_#{fnum}")
 loaded = get_all_threads(fnum, tlist, replies, 0)
 puts "Time to load #{loaded - time}"
 puts "Time to parse #{Time.now - time}"
